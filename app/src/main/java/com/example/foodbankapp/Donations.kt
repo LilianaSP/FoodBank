de911f211fd1ed4514f5994225d6baad0dcf5e3d
Emilio Berber <emilioberber@hotmail.com>
2023-09-23 00:53:11 -0600

    Adding Settings, Donations and CreatingPost asctivities

---
+++ app/src/main/java/com/example/foodbankapp/Donations.kt
@@ -0,0 +1,11 @@
+package com.example.foodbankapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Donations : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_donations)
+    }
+}
