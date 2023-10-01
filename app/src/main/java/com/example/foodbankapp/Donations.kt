310a5254afd1a5f53945e7579455c9b3bcd17915
Emilio Berber <emilioberber@hotmail.com>
2023-09-30 18:22:21 -0600

    Creating front-end for donations activity

--- app/src/main/java/com/example/foodbankapp/Donations.kt
+++ app/src/main/java/com/example/foodbankapp/Donations.kt
@@ -7,5 +7,6 @@ class Donations : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_donations)
+
     }
 }
