80b6044c784a72ae731024c336874831bce31ce6
Emilio Berber <emilioberber@hotmail.com>
2023-10-03 19:23:49 -0600

    Adding popup settings menu to donations act

--- app/src/main/java/com/example/foodbankapp/Donations.kt
+++ app/src/main/java/com/example/foodbankapp/Donations.kt
@@ -1,5 +1,6 @@
 package com.example.foodbankapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
@@ -12,16 +13,22 @@ class Donations : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_donations)
 
-        // Objetenemos el botón para desplegar los meses
+        // Obtenemos el botón para desplegar los meses
         val btnMes = findViewById<Button>(R.id.mont_donationms)
+        // obtenemos el botón para el settings ...:
+        val btnSettDonation = findViewById<Button>(R.id.dontations_navbar)
 
-        // Create a PopupMenu
+        // Create a PopupMenu for months
         val popupMenu = PopupMenu(this, btnMes)
+        // Create a PopupMenu for settings
+        val settingsPopupMenu = PopupMenu(this, btnSettDonation)
 
         // Inflate the menu with the month items
         popupMenu.menuInflater.inflate(R.menu.donations_month_menu, popupMenu.menu)
+        // Inflate the settings menu
+        settingsPopupMenu.menuInflater.inflate(R.menu.donations_settings_menu, settingsPopupMenu.menu)
 
-        // Set a listener for menu item clicks
+        // Set a listener for menu item clicks MONTHS
         popupMenu.setOnMenuItemClickListener { menuItem: MenuItem ->
             // Handle user selection
             when (menuItem.itemId) {
@@ -35,8 +42,26 @@ class Donations : AppCompatActivity() {
             }
         }
 
-        // Set a click listener for the button to show the Popup Menu
+        // Set a listener for settings menu item clicks SETTINGS
+        settingsPopupMenu.setOnMenuItemClickListener { menuItem: MenuItem ->
+            // Handle user selection for settings
+            when (menuItem.itemId) {
+                R.id.historial_donaciones -> {
+                    // Handle the "Historial de donaciones" selection
+                    val intent = Intent(this, HistorialDonations::class.java)
+                    startActivity(intent)
+                    return@setOnMenuItemClickListener true
+                }
+                else -> return@setOnMenuItemClickListener false
+            }
+        }
+
+        // Set a click listener for the button to show the Popup Menu MONTHS
         btnMes.setOnClickListener { view: View ->
+            popupMenu.show()
+        }
+        // Set a click listener for the button to show the Popup Menu SETTINGS
+        btnSettDonation.setOnClickListener { view: View ->
             popupMenu.show()
         }
     }
