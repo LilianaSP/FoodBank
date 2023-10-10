f40c4d2c8b18602d4b97cefd865fdd05221d91f5
Emilio Berber <emilioberber@hotmail.com>
2023-10-10 15:54:33 -0600

    Select and take picture for creating post act

--- app/src/main/java/com/example/foodbankapp/CreatingPost.kt
+++ app/src/main/java/com/example/foodbankapp/CreatingPost.kt
@@ -1,11 +1,93 @@
 package com.example.foodbankapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.PopupMenu
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class CreatingPost : AppCompatActivity() {
+
+    private lateinit var imageLauncher: ActivityResultLauncher<Intent>
+    private lateinit var imageToUpload: ImageView
+    private val REQUEST_IMAGE_CAPTURE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_creating_post)
+
+        imageToUpload = findViewById(R.id.imageToUpload)
+        val btnUploadPic = findViewById<Button>(R.id.btnUploadPic)
+
+        imageLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val selectedImageUri = result.data?.data
+                if (selectedImageUri != null) {
+                    val inputStream = contentResolver.openInputStream(selectedImageUri)
+                    val bitmap = BitmapFactory.decodeStream(inputStream)
+                    imageToUpload.setImageBitmap(bitmap)
+                }
+            }
+        }
+
+        btnUploadPic.setOnClickListener {
+            val popupMenu = PopupMenu(this, it)
+            popupMenu.menuInflater.inflate(R.menu.upload_pic_options_menu, popupMenu.menu)
+
+            popupMenu.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.menu_gallery -> {
+                        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                        imageLauncher.launch(intent)
+                        true
+                    }
+                    R.id.menu_camera -> {
+                        if (checkCameraPermission()) {
+                            openCamera()
+                        } else {
+                            requestCameraPermission()
+                        }
+                        true
+                    }
+                    else -> false
+                }
+            }
+
+            popupMenu.show()
+        }
+    }
+
+    private fun checkCameraPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestCameraPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_IMAGE_CAPTURE)
+    }
+
+    private fun openCamera() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imageToUpload.setImageBitmap(imageBitmap)
+        }
     }
 }
